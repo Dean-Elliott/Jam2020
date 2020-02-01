83e397da7b90715f6c8240fdb48fe6bd4d8f8c0e
Dean <dean94.elliott@gmail.com>
2020-02-01 15:08:29 -0500

    add base paintbrush guy

---
+++ Global Game Jam 2020/Assets/Code/Components/PaintBucket.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PaintBucket : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 size = new Vector3(1f, 1f, 1f);
+
+    [SerializeField]
+    private Vector3 offset = new Vector3(0f, 0f, 0f);
+
+    [SerializeField]
+    private Color color = Color.white;
+
+    [SerializeField]
+    private Renderer[] renderers = { };
+
+    public Bounds Bounds => new Bounds(transform.position + offset, size);
+    public Color Color => color;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = color;
+        Gizmos.DrawWireCube(Bounds.center, Bounds.size);
+    }
+
+    private void Update()
+    {
+        foreach (Renderer renderer in renderers)
+        {
+            renderer.material.color = color;
+        }
+    }
+}
--- Global Game Jam 2020/Assets/Code/Player/PlayerPaintbrush.cs
+++ Global Game Jam 2020/Assets/Code/Player/PlayerPaintbrush.cs
@@ -1,18 +1,60 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
-public class PlayerPaintbrush : MonoBehaviour
+public class PlayerPaintbrush : Player
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Renderer[] renderers = { };
+
+    [SerializeField]
+    private bool invert = false;
+
+    /// <summary>
+    /// The colour of the paint brush.
+    /// </summary>
+    public Color Color { get; set; } = Color.white;
+
+    protected override void OnUpdate()
     {
-        
+        PaintBucket[] buckets = FindObjectsOfType<PaintBucket>();
+        foreach (PaintBucket bucket in buckets)
+        {
+            if (bucket.Bounds.Contains(transform.position))
+            {
+                Color = bucket.Color;
+            }
+        }
+
+        foreach (Renderer renderer in renderers)
+        {
+            renderer.material.color = Color;
+        }
+
+        if (LeftTrigger > 0.2f)
+        {
+            Paint();
+        }
+
+        Vector2 rightStick = RightStick;
+        if (invert)
+        {
+            rightStick *= -1f;
+        }
+
+        float x = rightStick.x;
+        float magnitude = rightStick.magnitude;
+        if (rightStick.y < 0f)
+        {
+            magnitude *= -1f;
+            x *= -1f;
+        }
+
+        Vector3 eulerAngles = new Vector3(magnitude * 90f, x * 90f, 0f);
+        transform.eulerAngles = eulerAngles;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Paint()
     {
-        
+
     }
 }
