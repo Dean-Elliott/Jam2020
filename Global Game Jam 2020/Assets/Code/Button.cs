96af721bc5d8dfd64bfffed5047bb6eca8f6341e
Rowan Allcorn <rowanallcorn@gmail.com>
2020-02-01 13:58:54 -0500

    Rotatin Lazy Suzie

---
+++ Global Game Jam 2020/Assets/Code/Button.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+
+    public LazySuzie LazySuzie;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+
+        if (col.gameObject.tag == "Player")
+        {
+            LazySuzie.ForwardY = true;
+
+
+        }
+        /*
+        else
+        {
+           LazySuzie.ForwardY = false;
+        }
+        */
+    }
+
+        void OnTriggerExit(Collider col)
+        {
+            if (col.gameObject.tag == "Player")
+                {
+                LazySuzie.ForwardY = false;
+            }
+               
+            
+        }
+    
+}
---
+++ Global Game Jam 2020/Assets/Code/LazySuzie.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LazySuzie : MonoBehaviour
+{
+    //Rotational Speed
+    public float speed = 0f;
+
+    //Forward Direction
+   
+    public bool ForwardY = false;
+  
+
+    //Reverse Direction
+ 
+    public bool ReverseY = false;
+
+    private void OnEnable()
+    {
+        ForwardY = false;
+        ReverseY = false;
+    }
+    void Update()
+    {
+
+
+        //Forward Direction
+
+        if (ForwardY == true)
+        {
+            transform.Rotate(0, Time.deltaTime * speed, 0, Space.Self);
+        }
+     
+        //Reverse Direction
+     
+        if (ReverseY == true)
+        {
+            transform.Rotate(0, -Time.deltaTime * speed, 0, Space.Self);
+        }
+     
+
+    }
+
+
+    void OnTriggerEnter(Collider col)
+    {
+        
+        if (col.gameObject.tag == "Button")
+        {
+            ForwardY = true;
+
+
+        }
+        else
+        {
+            ForwardY = false;
+        }
+    }
+}
+
