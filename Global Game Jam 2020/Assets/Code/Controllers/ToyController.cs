9ee09a9278a9a6e44b64766a386b598105350b2c
DrewGodden <35704776+DrewGodden@users.noreply.github.com>
2020-02-01 13:54:16 -0500

    Implement basic toy placeholder
    Implemented basic toy script that tracks score and bonus time limit. Also added animations for toy enter and exit.

---
+++ Global Game Jam 2020/Assets/Code/Controllers/ToyController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToyController : MonoBehaviour
+{
+    private Animator animatorComponent;
+
+    [HideInInspector]
+    public float totalToyScore;
+
+    public bool developerModeActive = false;
+    
+    private float paintAccuracy;
+
+    public float bonusTimeLimit;
+    private float elapsingBonusTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animatorComponent = gameObject.GetComponent<Animator>();
+
+        elapsingBonusTime = bonusTimeLimit;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && developerModeActive == true)
+        {
+            ToyCompleted();
+        }
+
+        elapsingBonusTime -= Time.deltaTime;
+
+        //Debug.Log(elapsingBonusTime);
+
+        if (elapsingBonusTime < 0.0f)
+        {
+            elapsingBonusTime = 0.0f;
+        }
+    }
+
+    public void AddPieceScore(float pieceScore)
+    {
+        totalToyScore += pieceScore;
+    }
+
+    public void ToyCompleted()
+    {
+        totalToyScore += elapsingBonusTime + paintAccuracy;
+        animatorComponent.SetBool("ToyCompleted", true);
+
+        Debug.Log("Total toy score: " + totalToyScore);
+    }
+
+
+}
