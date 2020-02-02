bf70c41f7c4cc41787e1f01d3e774903dd715f6c
Dean <dean94.elliott@gmail.com>
2020-02-02 11:24:36 -0500

    New End Screen

--- Global Game Jam 2020/Assets/Code/SceneTransition.cs
+++ Global Game Jam 2020/Assets/Code/SceneTransition.cs
@@ -9,7 +9,7 @@ public class SceneTransition : MonoBehaviour
 
     public GameObject screw;
     public GameObject nail;
-    
+    public bool onScene = true;
 
     // Start is called before the first frame update
     void Start()
@@ -20,10 +20,22 @@ public class SceneTransition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (nail.GetComponent<Nail>().nailInStage == 2 && screw.GetComponent<Screw>().progress == 1f)
+        if (screw.GetComponent<Screw>().progress == 1f && nail.GetComponent<Nail>().nailInStage == 2 && onScene == true)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            SceneManager.LoadScene(0);
         }
+
+        if (screw.GetComponent<Screw>().progress == 1f)
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        if (nail.GetComponent<Nail>().nailInStage == 2 )
+        {
+            Application.Quit();
+        }
+
+
 
     }
 
