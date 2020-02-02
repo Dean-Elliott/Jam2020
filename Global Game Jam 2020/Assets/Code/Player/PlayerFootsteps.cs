6618668d6e287265f8db85a30371cff58d203666
Dean <dean94.elliott@gmail.com>
2020-02-02 14:31:14 -0500

    add footsteps to hammer

---
+++ Global Game Jam 2020/Assets/Code/Player/PlayerFootsteps.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFootsteps : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem footsteps;
+
+    private PlayerMovement playerMovement;
+
+    private void Awake()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    private void Update()
+    {
+        ParticleSystem.EmissionModule emission = footsteps.emission;
+        emission.enabled = playerMovement.IsGrounded && playerMovement.IsMoving;
+    }
+}
