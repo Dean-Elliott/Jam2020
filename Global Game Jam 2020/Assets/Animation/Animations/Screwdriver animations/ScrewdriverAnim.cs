1eeb9cccd082baba518d958be77f8788e1edc88d
Jackson Lanaus <38150077+380Jackson@users.noreply.github.com>
2020-02-02 14:36:47 -0500

    Animations

---
+++ Global Game Jam 2020/Assets/Animation/Animations/Screwdriver animations/ScrewdriverAnim.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrewdriverAnim : MonoBehaviour
+{
+    public PlayerScrewdriver PlayerS;
+    public PlayerMovement Player;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        anim.SetBool("IsScrewing", PlayerS.IsScrewingIn);
+        
+        anim.SetBool("IsWalking", Player.IsMoving);
+    }
+}
