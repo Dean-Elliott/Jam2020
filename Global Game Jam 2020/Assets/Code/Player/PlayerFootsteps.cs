9d494537a048e6c1cf9c5a2cd200cdf3d51f53e5
METOUR <dean94.elliott@gmail.com>
2020-02-05 00:49:06 -0500

    Updated Room & Added Footsteps

--- Global Game Jam 2020/Assets/Code/Player/PlayerFootsteps.cs
+++ Global Game Jam 2020/Assets/Code/Player/PlayerFootsteps.cs
@@ -7,6 +7,10 @@ public class PlayerFootsteps : MonoBehaviour
     [SerializeField]
     private ParticleSystem footsteps;
 
+    [SerializeField]
+    private ParticleSystem footsteps2;
+
+
     private PlayerMovement playerMovement;
 
     private void Awake()
@@ -17,6 +21,7 @@ public class PlayerFootsteps : MonoBehaviour
     private void Update()
     {
         ParticleSystem.EmissionModule emission = footsteps.emission;
+        ParticleSystem.EmissionModule emission2 = footsteps2.emission;
         emission.enabled = playerMovement.IsGrounded && playerMovement.IsMoving;
     }
 }
