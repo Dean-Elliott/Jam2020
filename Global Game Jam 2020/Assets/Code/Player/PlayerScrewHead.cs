8ec694fe3eda2d6125fdbdd616062cd2936222b8
METOUR <dean94.elliott@gmail.com>
2020-02-05 01:21:04 -0500

    Added ScrewDriver Particle Effect
    NOT FINISHED. Stricly added the effect and created a script.  Not too sure how to implement this one

---
+++ Global Game Jam 2020/Assets/Code/Player/PlayerScrewHead.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScrewHead : MonoBehaviour
+{
+    public PlayerScrewdriver ScrewDriver { get; private set; }
+
+    public AudioClip screwIn;
+    AudioSource audioSource;
+    private float screwingTime;
+    public GameObject screwEffect;
+
+    private void Awake()
+    {
+        ScrewDriver = GetComponentInParent<PlayerScrewdriver>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        //too soon
+        if (Time.time < screwingTime)
+        {
+            if (audioSource)
+            {
+                //audioSource.PlayOneShot(hammerHit, 0.7F);
+            }
+
+            return;
+        }
+
+        if (!ScrewDriver)
+        {
+            ScrewDriver = GetComponentInParent<PlayerScrewdriver>();
+        }
+
+        if (ScrewDriver && ScrewDriver.IsScrewingIn)
+        {
+            Screw screw = collision.GetComponentInParent<Screw>();
+            if (screw)
+            {
+                if (audioSource)
+                {
+                   // audioSource.PlayOneShot(hammerHit, 0.7F);
+                }
+
+               // screwingTime = Time.time + 0.25f;
+                
+              //  screw.screwingTime();
+
+                if (screwEffect)
+                {
+                    var newEffect = Instantiate(screwEffect, transform.position, Quaternion.identity);
+                   // Destroy(newEffect, 2f);
+                }
+            }
+        }
+    }
+}
